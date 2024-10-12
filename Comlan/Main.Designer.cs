19f627f8fb11751b47682d7be964fb2100ca3cfa
Miiraak <miiraak@feynlok.ch>
2024-10-12 17:58:55 +0200

    Add close button, update labels, and adjust UI elements
    - Replaced label1 with label2 and updated its properties to display "Comlan - v1.0.0" with a new font style and size.
    - Added a new buttonClose with an event handler buttonClose_Click to close the application and the connection.
    - Adjusted the location and size of richTextBoxChannel. Updated buttonAddFile and buttonSend to have no border.
    - Set the form's border style to None.
    - Commented out the error message display in the catch block of the message receiving function.
    - Added a new image resource icons8-close-24 for the close button and updated Main.resx, Resources.resx, and Resources.Designer.cs accordingly.

--- Comlan/Main.Designer.cs
+++ Comlan/Main.Designer.cs
@@ -33,20 +33,22 @@
             buttonAddFile = new Button();
             buttonSend = new Button();
             TextBoxWrite = new TextBox();
-            label1 = new Label();
+            label2 = new Label();
+            buttonClose = new Button();
             SuspendLayout();
             // 
             // richTextBoxChannel
             // 
-            richTextBoxChannel.Location = new Point(12, 23);
+            richTextBoxChannel.Location = new Point(12, 42);
             richTextBoxChannel.Name = "richTextBoxChannel";
             richTextBoxChannel.ReadOnly = true;
-            richTextBoxChannel.Size = new Size(386, 352);
+            richTextBoxChannel.Size = new Size(386, 333);
             richTextBoxChannel.TabIndex = 0;
             richTextBoxChannel.Text = "";
             // 
             // buttonAddFile
             // 
+            buttonAddFile.FlatAppearance.BorderSize = 0;
             buttonAddFile.FlatStyle = FlatStyle.Flat;
             buttonAddFile.Image = (Image)resources.GetObject("buttonAddFile.Image");
             buttonAddFile.Location = new Point(368, 381);
@@ -61,6 +63,7 @@
             buttonSend.BackColor = SystemColors.Control;
             buttonSend.BackgroundImage = (Image)resources.GetObject("buttonSend.BackgroundImage");
             buttonSend.BackgroundImageLayout = ImageLayout.Center;
+            buttonSend.FlatAppearance.BorderSize = 0;
             buttonSend.FlatStyle = FlatStyle.Flat;
             buttonSend.Location = new Point(320, 381);
             buttonSend.Name = "buttonSend";
@@ -78,15 +81,28 @@
             TextBoxWrite.Size = new Size(290, 22);
             TextBoxWrite.TabIndex = 5;
             // 
-            // label1
+            // label2
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 8);
-            label1.Name = "label1";
-            label1.Size = new Size(105, 14);
-            label1.TabIndex = 6;
-            label1.Text = "Global Channel";
+            label2.AutoSize = true;
+            label2.Font = new Font("Consolas", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(13, 11);
+            label2.Name = "label2";
+            label2.Size = new Size(160, 22);
+            label2.TabIndex = 7;
+            label2.Text = "Comlan - v1.0.0";
+            // 
+            // buttonClose
+            // 
+            buttonClose.BackColor = Color.Transparent;
+            buttonClose.FlatAppearance.BorderSize = 0;
+            buttonClose.FlatStyle = FlatStyle.Flat;
+            buttonClose.Image = Properties.Resources.icons8_close_24;
+            buttonClose.Location = new Point(368, 8);
+            buttonClose.Name = "buttonClose";
+            buttonClose.Size = new Size(30, 30);
+            buttonClose.TabIndex = 8;
+            buttonClose.UseVisualStyleBackColor = false;
+            buttonClose.Click += buttonClose_Click;
             // 
             // Main
             // 
@@ -94,13 +110,15 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
             ClientSize = new Size(410, 420);
-            Controls.Add(label1);
+            Controls.Add(buttonClose);
+            Controls.Add(label2);
             Controls.Add(TextBoxWrite);
             Controls.Add(buttonSend);
             Controls.Add(buttonAddFile);
             Controls.Add(richTextBoxChannel);
             Cursor = Cursors.Cross;
             Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
             MaximizeBox = false;
             Name = "Main";
             Text = "Lan Communication";
@@ -114,6 +132,7 @@
         private Button buttonAddFile;
         private Button buttonSend;
         private TextBox TextBoxWrite;
-        private Label label1;
+        private Label label2;
+        private Button buttonClose;
     }
 }
